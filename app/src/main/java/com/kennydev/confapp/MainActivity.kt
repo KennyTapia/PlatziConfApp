c25c9102ef2a78f4ce8442fe6b2318a6ee8a8af8
Kenny Tapia <ktapia@coresystems.io>
2021-01-18 18:04:25 -0600

    Initial commit
    Create default project structure
    for platziConfApp

---
+++ app/src/main/java/com/kennydev/confapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kennydev.confapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
