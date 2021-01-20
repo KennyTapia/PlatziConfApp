02961edbf240f9a667c5ba5e5ab8f0350c68d373
Kenny Tapia <ktapia@coresystems.io>
2021-01-20 16:12:08 -0600

    Add fragments needed for the app

---
+++ app/src/main/java/com/kennydev/confapp/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.kennydev.confapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kennydev.confapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
